58b33a4e200c074e6892d3177f7b02899fa18af1
Jiacheng Zhou <jianchenfutbol@gmail.com>
2023-10-24 17:17:49 +0200

    landing-page-creation

---
+++ src/pages/components/CalltoActionButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CalltoActionButton = () => {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">
+    Learn More
+  </button>
+  );
+};
+
+export default CalltoActionButton;
